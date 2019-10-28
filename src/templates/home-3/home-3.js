96b88da28676ac088da9020a31cfb0b0d9f17abd
MarianVytak <marianvytak@gmail.com>
2019-10-28 13:13:03 +0200

    Home-3: added adaptive.

--- src/templates/home-3/home-3.js
+++ src/templates/home-3/home-3.js
@@ -3,5 +3,13 @@ $('.home-3__slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
-    fade: true
+    fade: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
 });
